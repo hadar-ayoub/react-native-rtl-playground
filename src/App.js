5cd75e67fc9493e9e31dbd45b5748705dd67ec8b
Ayoub Hadar <aub.hadar@gmail.com>
2020-02-20 11:37:28 +0100

    migrate app to react native 0.61.5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import {combineReducers, createStore} from 'redux';
-import Module from './module'
-import settings from './module/reducer'
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import Module from "./module";
+import settings from "./module/reducer";
 
-const store = createStore(combineReducers({settings}), {});
+const store = createStore(combineReducers({ settings }), {});
 
 export default class App extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <Provider store={store}>
-                <Module/>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Module />
+      </Provider>
+    );
+  }
 }
-
--- src/module/reducer.js
+++ src/module/reducer.js
@@ -1,20 +1,20 @@
 const defaultState = {
-    isRTL:false
+  isRTL: false,
 };
 
 export default function complaintReducer(state = defaultState, action) {
-    switch (action.type) {
-        case "CHANGE_TO_AR":
-            return {
-                ...state,             // keep the existing state,
-                isRTL:true
-            };
-        case "CHANGE_TO_EN":
-            return {
-                ...state,             // keep the existing state,
-                isRTL:false
-            };
-        default:
-            return state;
-    }
-};
+  switch (action.type) {
+    case 'CHANGE_TO_AR':
+      return {
+        ...state, // keep the existing state,
+        isRTL: true,
+      };
+    case 'CHANGE_TO_EN':
+      return {
+        ...state, // keep the existing state,
+        isRTL: false,
+      };
+    default:
+      return state;
+  }
+}
